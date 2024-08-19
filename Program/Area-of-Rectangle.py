c94cdefa56a1c596a5c59fca67af814b2e6a0eb3
Aditya Khote <21csme004aditya.gmail.com>
2024-08-19 11:35:35 +0530

    Area of Rectangle

---
+++ Program/Area-of-Rectangle.py
@@ -0,0 +1,7 @@
+# 4 : WAP to calculate a Area of rectangle.
+
+length = float(input("Enter Length :- "))
+Width = float(input("Enter Width :- "))
+
+result = length * Width
+print("Area of Rectangle :- ",result)
