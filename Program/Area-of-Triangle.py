19142e6d2187f180154804327d5f863722f7fdd6
Aditya Khote <21csme004aditya.gmail.com>
2024-08-19 11:38:34 +0530

    Area of Triangle

---
+++ Program/Area-of-Triangle.py
@@ -0,0 +1,8 @@
+# 5 : WAP to calculate a Area of Triangle.
+
+base = float(input("Enter base :- "))
+height = float(input("Enter height :- "))
+
+result = 0.5 / base * height
+
+print("Area of Triangle :- ", result)
