57165e95a241d39a60403789f71d006d79e55a10
Aditya Khote <21csme004aditya.gmail.com>
2024-09-04 15:00:26 +0530

    update

--- Program/sum_of_digit.py
+++ Program/sum_of_digit.py
@@ -1,16 +1,20 @@
 #  TAKE A INPUT FROM USER AND AND FIND THE SUM OF DIGITS IN THE NUMBER 
 
-# num = input("Enter a number: ")
-# sum_of_digit = 0
+num = input("Enter a number: ")
+sumof = 0
 
-# for i in num:
-#     sum_of_digit += int(i)
+for i in num:
+    sumof += int(i)
 
-# print("Sum of digits:", sum_of_digit)
+print("Sum of digits:", sumof)
 
-a=int(input("Enter Range : - "))
-sum=0
-for i in range (a):
-    num = int(input("Enter Number : "))
-    sum += num
-print("Sum of digit :- ",sum)
+
+# a=int(input("Enter Range :-  "))
+# sum=0
+# if a > 0:
+#  for i in range (a):
+#     num = int(input("Enter Number : "))
+#     sum += num
+#  print("Sum of digit :- ",sum)
+# else:
+#  print("Enter invalid number")
